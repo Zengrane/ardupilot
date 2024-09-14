7374ec268a6362f1d51a87c5315085340ee46027
Zengrane <samuelsantucci04@gmail.com>
2024-09-14 13:03:40 +0930

    Script Modifications: Fixed bugs causing ardupilot not to run. Added scripts for search and tracking

---
+++ Tools/autotest/launch_arduplane.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+for i in $(seq 1 $1); do
+	I="$(($i-1))"
+	echo "Running instance $I"
+	port=$((14551+$I*10))
+	gnome-terminal --title="pilot $i" --working-directory=$HOME/Desktop/ardupilot/ArduPlane -e "/home/ubuntu/Desktop/ardupilot/Tools/autotest/sim_vehicle.py -v ArduPlane --instance=$I --sysid=$i --out=127.0.0.1:$port
+" &
+sleep 3
+done
+
+
+masters=""
+for i in $(seq 1 $1); do
+	I="$(($i-1))"
+	
+	port=$((14550 + $I*10))
+	masters="${masters} --master=127.0.0.1:$port"
+done
+
+echo $masters
+
+mavproxy.py ${masters} --map --console --load-module horizon  # --load module swarm # 
+
