6a04b32704a8d879bdc272574d78cc4706af9e48
Zengrane <samuelsantucci04@gmail.com>
2024-09-15 11:50:59 +0930

    Added additional comments

--- Tools/autotest/main.py
+++ Tools/autotest/main.py
@@ -24,22 +24,32 @@ seeker0 = seekerAgent("127.0.0.1:14551", 0)
 #seeker2 = seekerAgent("127.0.0.1:14571", 2)
 seekers = [seeker0]
 
-takeoff_location = dk.LocationGlobalRelative(-35.36487698, 149.17000667, 100)
+# Location where drone should take off and loiter
+takeoff_location = dk.LocationGlobalRelative(-35.35898964, 149.16463160, 100)
+# Home location of the drone
 home_lat_lon = np.array([-35.36341649, 149.16525123])
+# Defines home location of the drone representing a geographic location
+# relative to the drone 
 home_location = dk.LocationGlobalRelative(home_lat_lon[0], home_lat_lon[1], 100)
-target_lat_lon = (-35.33849343, 149.17902953) 
+# Target location for drone to fly to
+target_lat_lon = (-35.36290851, 149.15882125) 
+# Defines the target location as a geographic location relative to drone's
+# position
 target_location = dk.LocationGlobalRelative(target_lat_lon[0], target_lat_lon[1], 100)
 
-# Assign target and home location to seeker
+
+# Assign target and home location to each seeker
 for idx in range(n_agents):
     seekers[idx].home_lat_lon = home_lat_lon
 
 # Takeoff all drones
 takeoff_status = False
+# Loops until all drones have taken off
 while True:
     if takeoff_status:
         break
 
+    # Iterates over each drone
     for idx in range(n_agents):
         # Drones will fly to here and loiter about here
         if not seekers[idx].vehicle.armed:  # check if takeoff occurred
@@ -47,7 +57,9 @@ while True:
             seekers[idx].mode = VehicleMode("GUIDED")
             sleep(1)
 
+    # Iterates oveer each drone
     for idx in range(n_agents):
+        # Commands the drones to go to the takeoff location
         seekers[idx].vehicle.simple_goto(takeoff_location)
         print(" Altitude: ", seekers[idx].vehicle.location.global_relative_frame.alt)
         # Break and return from function just below target altitude.
@@ -62,19 +74,24 @@ while True:
 
 print("all drones taken off")
 
+# An array that tracks which drones have reached the target
 agents_done = np.zeros(shape=n_agents, dtype=bool)
-
+# Calculate east and north coordinates relative to the home location
 dE_target_from_home, dN_target_from_home = calc_east_north(home_lat_lon[0], home_lat_lon[1],
                                                            target_lat_lon[0], target_lat_lon[1])
+# Initialises the target position in north, east, and down coordinates
 target_pos_NED_init = np.array([dN_target_from_home, dE_target_from_home, -100.])
 
+# Main loop to navigate the drones
 while True:
-
+    # Iterate over each drone
     for idx in range(n_agents):
+        # Computes east and north displacements from the home location
         dE, dN = calc_east_north(home_lat_lon[0], home_lat_lon[1],
                                  seekers[idx].vehicle.location.global_relative_frame.lat,
                                  seekers[idx].vehicle.location.global_relative_frame.lon)
 
+        # Calculates drone's position in NED coordinates
         agent_pos_NED = np.array([dN, dE, -seekers[idx].vehicle.location.global_relative_frame.alt])
         target_pos_NED = target_pos_NED_init   # static target
 
@@ -87,9 +104,11 @@ while True:
         """
         # seekers[idx].set_attitude(roll_angle=10., pitch_angle=1., thrust=0.5, duration=10)
 
+        # Checks if the distance to target is less or equal to 50 meters of the target
         distance_to_target = get_distance_meters(agent_pos_NED, target_pos_NED)
         if distance_to_target <= 50.:
             if not agents_done[idx]:
+                # Updates agents_done indicating drone has reached the target
                 agents_done[idx] = True
                 break
 
